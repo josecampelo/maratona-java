652a996c9c9e3c2df4ad1823ea5902ea12752dc2
Campelo <josecampelojr@gmail.com>
2025-03-25 09:36:13 -0300

    Aula 10: Tipos primitivos parte 1 - Convenções de variáveis

---
+++ src/br/com/josecampelo/maratonajava/introducao/tiposprimitivos/Aula01.java
@@ -0,0 +1,10 @@
+package br.com.josecampelo.maratonajava.introducao.tiposprimitivos;
+
+public class Aula01 {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 18;
+        System.out.println(idade);
+        System.out.println("A idade é " + idade + " anos");
+    }
+}
