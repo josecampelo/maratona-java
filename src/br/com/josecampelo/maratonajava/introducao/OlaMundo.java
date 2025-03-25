d40e9c1c82d6c85440df57b0aa4fd48263ac9159
Campelo <josecampelojr@gmail.com>
2025-03-25 09:05:27 -0300

    Aula 09 - Comentários de uma linha, múltiplas linhas e JavaDoc

--- src/br/com/josecampelo/maratonajava/introducao/OlaMundo.java
+++ src/br/com/josecampelo/maratonajava/introducao/OlaMundo.java
@@ -1,6 +1,16 @@
 package br.com.josecampelo.maratonajava.introducao;
 
+/**
+ *  Isso é um comentário <b>javadoc</b>
+ */
 public class OlaMundo {
+    // Isso é um comentário de uma linha
+
+    /*
+    Isso é um comentário
+    de múltiplas
+    linhas
+     */
     public static void main (String[] args) {
         System.out.println("Olá mundo!");
     }
