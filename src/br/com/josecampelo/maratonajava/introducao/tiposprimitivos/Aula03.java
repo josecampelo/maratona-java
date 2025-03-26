1b809f2c09d12f5980d6e40d5f575de73f061203
Campelo <josecampelojr@gmail.com>
2025-03-26 07:37:21 -0300

    Aula 12: Tipos primitivos parte 3 - Casting

---
+++ src/br/com/josecampelo/maratonajava/introducao/tiposprimitivos/Aula03.java
@@ -0,0 +1,15 @@
+package br.com.josecampelo.maratonajava.introducao.tiposprimitivos;
+
+public class Aula03 {
+    public static void main(String[] args) {
+        int numeroInteiro = 321321;
+        double numeroDecimal = 789745.531;
+
+        System.out.println("Valor do número inteiro: " + numeroInteiro);
+        System.out.println("Valor do número decimal: " + numeroDecimal);
+
+        numeroInteiro = (int) numeroDecimal;
+        System.out.println("Valor do número decimal dentro do inteiro com casting: " + numeroInteiro);
+        System.out.println("A parte decimal (0.531) é perdida, pois int só armazena números inteiros.");
+    }
+}
