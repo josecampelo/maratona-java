c761ad0bca17c6ec1e6cd5cefd03ec05d28f9046
Campelo <josecampelojr@gmail.com>
2025-03-26 08:17:58 -0300

    Aula 13: Tipos primitivos parte 4 - Strings

---
+++ src/br/com/josecampelo/maratonajava/introducao/tiposprimitivos/Aula04.java
@@ -0,0 +1,11 @@
+package br.com.josecampelo.maratonajava.introducao.tiposprimitivos;
+
+public class Aula04 {
+    public static void main(String[] args) {
+        String frase = "Digitando alguma frase aleatório para representar uma String";
+        String nome = "José Campelo";
+
+        System.out.println(frase);
+        System.out.println("Meu nome é " + nome);
+    }
+}
